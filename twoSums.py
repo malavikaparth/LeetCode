d5fec037b34e3ac496a907add27f3f6623b9cead
Malavika <parth.malavika@gmail.com>
2023-11-13 21:26:58 +0100

    initial commit

---
+++ twoSums.py
@@ -0,0 +1,21 @@
+
+#Time complexity: O(n)
+#Space complexity: O(n)
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+
+#Time complexity: O(n^2)
+#Space complexity: O(1)
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
